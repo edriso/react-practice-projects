778e24c5e9149a6dd6c7be5a4f393b893ceac7a4
edriso <edriso.dev@gmail.com>
2023-03-19 22:23:44 +0200

    app structure

--- backroads/src/App.js
+++ backroads/src/App.js
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>hello, world!</p>
-      </header>
+      <Header />
     </div>
   );
 }

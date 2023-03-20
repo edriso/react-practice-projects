aea9311ac94c4d0814c24e2a131331a3e9e85e89
edriso <edriso.dev@gmail.com>
2023-03-20 14:20:58 +0200

    update Tours component

--- backroads/src/data.js
+++ backroads/src/data.js
@@ -1,3 +1,8 @@
+import tourImg1 from "./assets/images/tour-1.jpeg";
+import tourImg2 from "./assets/images/tour-2.jpeg";
+import tourImg3 from "./assets/images/tour-3.jpeg";
+import tourImg4 from "./assets/images/tour-4.jpeg";
+
 export const pageLinks = ["home", "about", "services", "tours"];
 
 export const socialLinks = [
@@ -33,5 +38,48 @@ export const offeredServices = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
     icon: "fas fa-socks fa-fw",
+  },
+];
+
+export const featuredTours = [
+  {
+    title: "Tibet Adventure",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "August 26th, 2023",
+    location: "China",
+    duration: "6 Days",
+    cost: "2100",
+    cover: tourImg1,
+  },
+  {
+    title: "Best Of Indonesia",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "October 1th, 2023",
+    location: "Indonesia",
+    duration: "11 Days",
+    cost: "1400",
+    cover: tourImg2,
+  },
+  {
+    title: "Explore Hong Kong",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "September 15th, 2023",
+    location: "Hong Kong",
+    duration: "8 Days",
+    cost: "5000",
+    cover: tourImg3,
+  },
+  {
+    title: "Kenya Highlights",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "December 5th, 2023",
+    location: "Kenya",
+    duration: "20 Days",
+    cost: "3300",
+    cover: tourImg4,
   },
 ];

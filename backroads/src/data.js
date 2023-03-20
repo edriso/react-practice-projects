004d09f56b222f0263a7a301ac60c33ef864ebe5
edriso <edriso.dev@gmail.com>
2023-03-20 12:09:13 +0200

    add navbar logo, links, and icons

---
+++ backroads/src/data.js
@@ -0,0 +1,16 @@
+export const pageLinks = ["home", "about", "services", "tours"];
+
+export const navIcons = [
+  {
+    href: "https://www.facebook.com",
+    icon: "fa-facebook",
+  },
+  {
+    href: "https://www.twitter.com",
+    icon: "fa-twitter",
+  },
+  {
+    href: "https://www.squarespace.com",
+    icon: "fa-squarespace",
+  },
+];

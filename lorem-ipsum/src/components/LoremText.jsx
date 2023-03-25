84d19613b3fb8fea6340c2cae6194e0ee8c8e195
edriso <edriso.dev@gmail.com>
2023-03-25 12:47:27 +0200

    add nanoid package

--- lorem-ipsum/src/components/LoremText.jsx
+++ lorem-ipsum/src/components/LoremText.jsx
@@ -1,8 +1,11 @@
+import { nanoid } from "nanoid";
+
 export function LoremText({ paragraphs }) {
   return (
     <article className="lorem-text">
-      {paragraphs.map((item, i) => {
-        return <p key={i}>{item}</p>;
+      {paragraphs.map((item) => {
+        const id = nanoid();
+        return <p key={id}>{item}</p>;
       })}
     </article>
   );

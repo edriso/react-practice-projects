2c71e0aaa0a7ba12b2f3c66e33305de8e6874d21
edriso <edriso.dev@gmail.com>
2023-03-25 14:43:00 +0200

    change button color depend on color brightness

--- color-generator/src/components/Form.jsx
+++ color-generator/src/components/Form.jsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from "react";
+import Values from "values.js";
 
 export function Form({ generateColors }) {
   const [color, setColor] = useState("#645cff");
+  const [brightness, setBrightness] = useState(
+    new Values("#645cff").getBrightness()
+  );
+
+  const handleChange = (e) => {
+    setColor(e.target.value);
+    setBrightness(new Values(color).getBrightness());
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,19 +23,18 @@ export function Form({ generateColors }) {
 
   return (
     <form className="color-form" onSubmit={handleSubmit}>
-      <input
-        type="color"
-        name="color"
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
-      />
+      <input type="color" name="color" value={color} onChange={handleChange} />
       <input
         type="text"
         placeholder={color}
         value={color}
-        onChange={(e) => setColor(e.target.value)}
+        onChange={handleChange}
       />
-      <button type="submit" className="btn" style={{ backgroundColor: color }}>
+      <button
+        type="submit"
+        className="btn"
+        style={{ backgroundColor: color, color: brightness > 75 && "#000" }}
+      >
         Generate
       </button>
     </form>

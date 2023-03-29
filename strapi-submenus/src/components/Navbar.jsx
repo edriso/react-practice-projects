56596a73bbdef4389167664da8906df8a128b955
edriso <edriso.dev@gmail.com>
2023-03-29 17:55:13 +0200

    setup navbar structure

--- strapi-submenus/src/components/Navbar.jsx
+++ strapi-submenus/src/components/Navbar.jsx
@@ -1,5 +1,26 @@
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "../AppContext";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <h3 className="logo">Stripe</h3>
+
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+
+        <div className="nav-links">
+          <button className="nav-link">products</button>
+          <button className="nav-link">solutions</button>
+          <button className="nav-link">resources</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;

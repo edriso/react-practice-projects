f6c03269555dd70dbd090574476b77476c82d694
edriso <edriso.dev@gmail.com>
2023-04-12 08:56:17 +0200

    add action conditions

--- cart/src/reducer/reducer.js
+++ cart/src/reducer/reducer.js
@@ -1,9 +1,49 @@
 import { TYPES } from "./actions";
-// import cartItems from "../assets/data";
+import cartItems from "../assets/data";
 
 const reducer = (state, action) => {
   if (action.type === TYPES.CLEAR_CART) {
     return { ...state, cart: [] };
+  }
+
+  if (action.type === TYPES.INCREASE_ITEM) {
+    const updatedCart = state.cart.filter((item) => {
+      if (item.id === action.payload.id) {
+        item.amount += 1;
+      }
+      return item;
+    });
+    return { ...state, cart: updatedCart };
+  }
+
+  if (action.type === TYPES.DECREASE_ITEM) {
+    const updatedCart = state.cart.filter((item) => {
+      if (item.id === action.payload.id && item.amount <= 1) {
+        return;
+      }
+
+      if (item.id === action.payload.id) {
+        item.amount -= 1;
+      }
+      return item;
+    });
+    return { ...state, cart: updatedCart };
+  }
+
+  if (action.type === TYPES.DISPLAY_ITEMS) {
+    return { ...state, cart: cartItems };
+  }
+
+  if (action.type === TYPES.REMOVE_ITEM) {
+    const updatedCart = state.cart.filter(
+      (item) => item.id !== action.payload.id
+    );
+
+    return { ...state, cart: updatedCart };
+  }
+
+  if (action.type === TYPES.LOADING) {
+    return { ...state, loading: true };
   }
 
   throw new Error(`No matching "${action.type}" - action type`);

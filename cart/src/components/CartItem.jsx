fcffcc9574e35a762c6d0c2b7d8bb71f4b0625b1
edriso <edriso.dev@gmail.com>
2023-04-12 10:41:30 +0200

    remove comments

--- cart/src/components/CartItem.jsx
+++ cart/src/components/CartItem.jsx
@@ -9,19 +9,15 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div>
         <h5>{title}</h5>
         <span className="item-price">${price}</span>
-        {/* remove button */}
         <button className="remove-btn" onClick={() => removeItem(id)}>
           remove
         </button>
       </div>
       <div>
-        {/* increase amount */}
         <button className="amount-btn" onClick={() => increaseItem(id)}>
           <FaChevronUp className="amount-icon" />
         </button>
-        {/* amount */}
         <span className="amount">{amount}</span>
-        {/* decrease amount */}
         <button className="amount-btn" onClick={() => decreaseItem(id)}>
           <FaChevronDown className="amount-icon" />
         </button>

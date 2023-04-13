7462d208feaeff73f97e18e4e425628044ba8328
edriso <edriso.dev@gmail.com>
2023-04-13 16:55:40 +0200

    show prev/next review

--- reviews/src/App.jsx
+++ reviews/src/App.jsx
@@ -6,6 +6,32 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = reviews[index];
 
+  const checkIndex = (index) => {
+    if (index > reviews.length - 1) {
+      return 0;
+    }
+    if (index < 0) {
+      return reviews.length - 1;
+    }
+    return index;
+  };
+
+  const nextReview = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      // return currentIndex === reviews.length - 1 ? 0 : currentIndex + 1;
+      return checkIndex(newIndex);
+    });
+  };
+
+  const prevReview = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      // return currentIndex === 0 ? reviews.length - 1 : currentIndex - 1;
+      return checkIndex(newIndex);
+    });
+  };
+
   return (
     <main>
       <article className="review">
@@ -19,6 +45,15 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevReview}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextReview}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   );

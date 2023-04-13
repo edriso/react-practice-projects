243c757c941c8dbe30072919cc45714ad6e7ba5b
edriso <edriso.dev@gmail.com>
2023-04-13 17:07:45 +0200

    make component for reviews

--- reviews/src/App.jsx
+++ reviews/src/App.jsx
@@ -1,72 +1,9 @@
-import { useState } from "react";
-import reviews from "./data";
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import Reviews from "./components/Reviews";
 
 const App = () => {
-  const [index, setIndex] = useState(0);
-  const { name, job, image, text } = reviews[index];
-
-  const checkIndex = (index) => {
-    if (index > reviews.length - 1) {
-      return 0;
-    }
-    if (index < 0) {
-      return reviews.length - 1;
-    }
-    return index;
-  };
-
-  const nextReview = () => {
-    setIndex((currentIndex) => {
-      const newIndex = currentIndex + 1;
-      // return currentIndex === reviews.length - 1 ? 0 : currentIndex + 1;
-      return checkIndex(newIndex);
-    });
-  };
-
-  const prevReview = () => {
-    setIndex((currentIndex) => {
-      const newIndex = currentIndex - 1;
-      // return currentIndex === 0 ? reviews.length - 1 : currentIndex - 1;
-      return checkIndex(newIndex);
-    });
-  };
-
-  const randomReview = () => {
-    let randomIndex = Math.floor(Math.random() * reviews.length);
-    if (randomIndex === index) {
-      randomIndex = randomIndex + 1;
-    }
-    setIndex(checkIndex(randomIndex));
-  };
-
   return (
     <main>
-      <article className="review">
-        <div className="img-container">
-          <img src={image} alt={name} className="person-img" />
-          <span className="quote-icon">
-            <FaQuoteRight />
-          </span>
-        </div>
-
-        <h4 className="author">{name}</h4>
-        <p className="job">{job}</p>
-        <p className="info">{text}</p>
-
-        <div className="btn-container">
-          <button className="prev-btn" onClick={prevReview}>
-            <FaChevronLeft />
-          </button>
-          <button className="next-btn" onClick={nextReview}>
-            <FaChevronRight />
-          </button>
-        </div>
-
-        <button className="btn btn-hipster" onClick={randomReview}>
-          Surprise me
-        </button>
-      </article>
+      <Reviews />
     </main>
   );
 };
---
+++ reviews/src/components/Reviews.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import reviews from "../data";
+
+const Reviews = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = reviews[index];
+
+  const checkIndex = (index) => {
+    if (index > reviews.length - 1) {
+      return 0;
+    }
+    if (index < 0) {
+      return reviews.length - 1;
+    }
+    return index;
+  };
+
+  const nextReview = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      // return currentIndex === reviews.length - 1 ? 0 : currentIndex + 1;
+      return checkIndex(newIndex);
+    });
+  };
+
+  const prevReview = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      // return currentIndex === 0 ? reviews.length - 1 : currentIndex - 1;
+      return checkIndex(newIndex);
+    });
+  };
+
+  const randomReview = () => {
+    let randomIndex = Math.floor(Math.random() * reviews.length);
+    if (randomIndex === index) {
+      randomIndex = randomIndex + 1;
+    }
+    setIndex(checkIndex(randomIndex));
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+
+      <div className="btn-container">
+        <button className="prev-btn" onClick={prevReview}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextReview}>
+          <FaChevronRight />
+        </button>
+      </div>
+
+      <button className="btn btn-hipster" onClick={randomReview}>
+        Surprise me
+      </button>
+    </article>
+  );
+};
+export default Reviews;

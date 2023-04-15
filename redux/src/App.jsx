308efdaff5d8893f62bb77f69381e1b9455904e0
edriso <edriso.dev@gmail.com>
2023-04-15 13:52:55 +0200

    use createAsyncThunk to fetch items async

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,17 +1,29 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import Modal from "./components/Modal";
 
 function App() {
   const { isOpen } = useSelector((state) => state.modal);
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
 
   return (
     <main>
